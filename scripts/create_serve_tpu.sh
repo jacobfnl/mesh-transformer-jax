5003d800a966533d80c4fa81800ec0b691a2d8d6
jacobfnl <jacobfennell@jacobfennell.com>
2021-08-21 17:44:36 -0700

    Testing a slim-weights deployment on a v2-8

--- scripts/create_serve_tpu.sh
+++ scripts/create_serve_tpu.sh
@@ -1,3 +1,3 @@
-gcloud alpha compute tpus tpu-vm create "$1" --zone europe-west4-a --accelerator-type v3-8 --version v2-alpha
+gcloud alpha compute tpus tpu-vm create "$1" --zone us-central1-b --accelerator-type v2-8 --version v2-alpha --premptible
 sleep 120
-gcloud alpha compute tpus tpu-vm ssh "$1" --zone europe-west4-a --command connor@sparse:~/kindiana/mesh-transformer-jax --command="$(< scripts/deploy_server.sh)"
+gcloud alpha compute tpus tpu-vm ssh "$1" --zone us-central1-b --command illogical@ill20:~/word_of_the_future/mesh-transformer-jax --command="$(< scripts/deploy_server.sh)"
--- scripts/deploy_server.sh
+++ scripts/deploy_server.sh
@@ -3,9 +3,9 @@ set -e
 
 rm -r mesh-transformer-jax || true
 
-git clone https://github.com/kingoflolz/mesh-transformer-jax
+git clone https://github.com/jacobfnl/mesh-transformer-jax
 pip install -r mesh-transformer-jax/requirements.txt
-pip install mesh-transformer-jax/ jax==0.2.12
+pip install mesh-transformer-jax/
 
 pushd mesh-transformer-jax || exit
-screen -d -m python3 device_serve.py --config configs/6B_roto_256.json
+screen -d -m python3 device_serve.py --config configs/6B_aw_slim.json
