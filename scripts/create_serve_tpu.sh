b1e4503cb4ebf4b81c38de3b1d0abe3c7c3ce11e
jacobfnl <jacobfennell@jacobfennell.com>
2021-08-21 18:10:45 -0700

    Spelling of "preemptible" and pad sleep by a minute

--- scripts/create_serve_tpu.sh
+++ scripts/create_serve_tpu.sh
@@ -1,3 +1,3 @@
-gcloud alpha compute tpus tpu-vm create "$1" --zone us-central1-b --accelerator-type v2-8 --version v2-alpha --premptible
-sleep 120
+gcloud alpha compute tpus tpu-vm create "$1" --zone us-central1-b --accelerator-type v2-8 --version v2-alpha --preemptible
+sleep 180
 gcloud alpha compute tpus tpu-vm ssh "$1" --zone us-central1-b --command illogical@ill20:~/word_of_the_future/mesh-transformer-jax --command="$(< scripts/deploy_server.sh)"
